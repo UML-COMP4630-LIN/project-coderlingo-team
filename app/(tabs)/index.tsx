2361ebfec0979fbdde5b71cb9b391eabaeaeb0c0
Rohan Mallu <rmallu21@gmail.com>
2025-03-21 20:45:44 -0400

    Background color change.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Coderlingo Official App. More Coming Soon.</Text>
+      <Text style={styles.text}>Coderlingo Official App. More Coming Soon.</Text>
     </View>
   );
+
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#000',
+  },
+});
